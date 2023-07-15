6d47d706af45ccb13a7df870a5a50a53ffe8d533
TeppuSan <StarVampire.teppu@gmail.com>
2023-07-16 00:56:52 +0900

    カレンダーでイベントを作れるようにしてそのイベントの詳細情報を見れるようにしました

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,12 @@
 import Navbar from '@/components/elements/Navbar';
+import Callendar from './callendar';
 
 export default function Home() {
   return (
     <>
       <main>
         <Navbar />
+        <Callendar />
       </main>
     </>
   );
