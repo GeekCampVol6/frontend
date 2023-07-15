e33140d8179264de9ea0fb0b9a394d97a012079a
me990928 <me990928@gmail.com>
2023-07-16 04:28:08 +0900

    add firebase regist Thumnail

--- src/firebase/firebase.jsx
+++ src/firebase/firebase.jsx
@@ -4,6 +4,7 @@ import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore"
 import "firebase/auth"
 import { getAuth } from "firebase/auth";
+import { getStorage, ref } from "firebase/storage";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -25,3 +26,4 @@ const app = initializeApp(firebaseConfig);
 
 export const db = getFirestore(app);
 export const au = getAuth(app);
+export const storage = getStorage();
